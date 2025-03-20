d10824ef54ccbb47a0d2636558e0d2d7fd9c051d
DFran49 <francresmartin@gmail.com>
2025-03-21 00:46:58 +0100

    Reproduccion de radio y barra de minutos y horas

--- app/src/main/java/com/nemo/imaginaradio/views/MainActivity.kt
+++ app/src/main/java/com/nemo/imaginaradio/views/MainActivity.kt
@@ -1,21 +1,19 @@
 package com.nemo.imaginaradio.views
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.nemo.imaginaradio.R
+import com.nemo.imaginaradio.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        initializeBinding()
+        setContentView(binding.root)
+    }
+
+    private fun initializeBinding() {
+        binding = ActivityMainBinding.inflate(layoutInflater)
     }
 }
