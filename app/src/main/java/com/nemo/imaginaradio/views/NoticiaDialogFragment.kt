09710cc2acf185895ef3fce77dcef0658f33407f
NessyDaBest <nessblack03@gmail.com>
2025-03-26 18:59:16 +0100

    Dialog fragment + Fragment Home actualizado

---
+++ app/src/main/java/com/nemo/imaginaradio/views/NoticiaDialogFragment.kt
@@ -0,0 +1,18 @@
+package com.nemo.imaginaradio.views
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import com.nemo.imaginaradio.R
+
+class NoticiaDialogFragment : DialogFragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.noticia_dialog, container, false)
+    }
+}
