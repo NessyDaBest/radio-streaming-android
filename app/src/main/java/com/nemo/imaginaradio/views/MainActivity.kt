186b01dde7187eaebd1d588941035eb156363c63
DFran49 <francresmartin@gmail.com>
2025-04-24 23:21:58 +0200

    Merge con rama_martin

--- app/src/main/java/com/nemo/imaginaradio/views/MainActivity.kt
+++ app/src/main/java/com/nemo/imaginaradio/views/MainActivity.kt
@@ -5,10 +5,17 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
 import com.nemo.imaginaradio.R
+import androidx.navigation.fragment.NavHostFragment
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+import androidx.navigation.ui.NavigationUI
+import com.nemo.imaginaradio.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity(){
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
@@ -17,5 +24,22 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        startBinding()
+        setupNavbar()
+    }
+
+    fun getNavController(): NavController{
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.fragment_container) as NavHostFragment
+        return navHostFragment.navController
+    }
+
+    private fun startBinding(){
+        binding = ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    private fun setupNavbar(){
+        NavigationUI.setupWithNavController(binding.mainNavbar, getNavController())
     }
 }
