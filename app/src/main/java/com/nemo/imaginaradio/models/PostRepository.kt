3abaa6b26719c1aea44163190dc46cdcd9263933
THElib03 <theliberator03@gmail.com>
2025-05-06 01:07:48 +0200

    Code cleanup of API handler

--- app/src/main/java/com/nemo/imaginaradio/models/PostRepository.kt
+++ app/src/main/java/com/nemo/imaginaradio/models/PostRepository.kt
@@ -1,7 +1,6 @@
 package com.nemo.imaginaradio.models
 
 import com.squareup.moshi.Moshi
-import com.squareup.moshi.Types
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 
@@ -10,40 +9,14 @@ class PostRepository {
     private val postApi:PostApi = Retrofit.Builder().baseUrl("https://www.imaginaradio.cat/wp-json/wp/v2/").addConverterFactory(
         MoshiConverterFactory.create(moshi)).build().create(PostApi::class.java)
 
-    val postRawListType = Types.newParameterizedType(List::class.java, PostRaw::class.java)
-    val postAdapter = moshi.adapter<List<PostRaw>>(postRawListType)
-
-    //suspend fun getCategoryPosts(category: Int, perPage: Int = 10): List<Post> = postApi.getCategoryPosts(category = category, perPage = perPage)
     suspend fun getCategoryPosts(category: Int, perPage: Int = 10): List<Post>{
-        val response: List<PostRaw> = postApi.getCategoryPosts(category = category, perPage = perPage)
-
-        return response.map{ post ->
-            Post(
-                id = post.id,
-                title = post.title.rendered,
-                content = post.content.rendered,
-                date = post.date,
-                categories = post.categories
-            )
-        }
+        return arrayParse(postApi.getCategoryPosts(category = category, perPage = perPage))
     }
 
-    //suspend fun getLastPosts(perPage: Int = 10): List<Post> = postApi.getLastPosts(perPage = perPage)
     suspend fun getLastPosts(perPage: Int = 10): List<Post>{
-        val response: List<PostRaw> = postApi.getLastPosts(perPage = perPage)
-
-        return response.map{ post ->
-            Post(
-                id = post.id,
-                title = post.title.rendered,
-                content = post.content.rendered,
-                date = post.date,
-                categories = post.categories
-            )
-        }
+        return arrayParse(postApi.getLastPosts(perPage = perPage))
     }
 
-    //suspend fun getPostById(id: Int): Post = postApi.getPostById(id)
     suspend fun getPostById(id: Int): Post{
         val response: PostRaw = postApi.getPostById(id = id)
 
@@ -54,5 +27,19 @@ class PostRepository {
             date = response.date,
             categories = response.categories
         )
+    }
+
+
+    //Parsing posts array response
+    fun arrayParse(res: List<PostRaw>): List<Post>{
+        return res.map{ post ->
+            Post(
+                id = post.id,
+                title = post.title.rendered,
+                content = post.content.rendered,
+                date = post.date,
+                categories = post.categories
+            )
+        }
     }
 }
