30f639fefd767cedbadb7c12b564ac78986fa21e
DFran49 <francresmartin@gmail.com>
2025-05-16 15:40:52 +0200

    Icono y fixeo reproductor flotante

--- app/src/main/java/com/nemo/imaginaradio/views/MainActivity.kt
+++ app/src/main/java/com/nemo/imaginaradio/views/MainActivity.kt
@@ -9,6 +9,7 @@ import android.view.View
 import android.view.animation.DecelerateInterpolator
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.doOnPreDraw
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import com.nemo.imaginaradio.R
@@ -23,8 +24,13 @@ class MainActivity : AppCompatActivity() {
         setTheme(R.style.Theme_ImaginaRadio)
         super.onCreate(savedInstanceState)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        startBinding()
+        setupNavbar()
+
         setContentView(binding.root)
+        hideShowNavBar()
+
+        //setContentView(binding.root)
 
         // Configura NavController
         val navHost = supportFragmentManager
@@ -38,6 +44,39 @@ class MainActivity : AppCompatActivity() {
                 animateSplash()
             }
             true
+        }
+    }
+
+    fun getNavController(): NavController {
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.fragment_container) as NavHostFragment
+        return navHostFragment.navController
+    }
+
+    private fun startBinding(){
+        binding = ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    private fun setupNavbar(){
+        NavigationUI.setupWithNavController(binding.mainNavbar, getNavController())
+    }
+
+    private fun hideShowNavBar() {
+        val navController = getNavController()
+        val floatingPlayer: View = findViewById(R.id.floating_player_fragment_container)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.homeFragment -> {
+                    // Primer fragmento: ocultar minireproductor
+                    floatingPlayer.visibility = View.GONE
+                }
+
+                else -> {
+                    // Opcional: comportamiento por defecto
+                    floatingPlayer.visibility = View.VISIBLE
+                }
+            }
         }
     }
 
